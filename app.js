d013959e35ef6e95286d2241d577b65d2b192a89
Alberto Urbano <hunterxyz@gmail.com>
2016-04-04 18:23:58 +0200

    jshint

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({limit: '15mb'}));
 
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.listen(port, ipaddress, function() {
