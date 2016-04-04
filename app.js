1e721d7c6f0160ebe54ec1e63927be93f550ff30
Alberto Urbano <hunterxyz@gmail.com>
2016-04-04 13:13:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var express = require('express');
+var SlackClient = require('slack-client');
+
+
+var tommy = function(){
+
+    var slackClient = new SlackClient('xoxb-31735152998-FyaXBNNVWzJ02aqkbqpWtp5t');
+
+    var bot; // Track bot user .. for detecting messages by yourself
+
+    slackClient.on('loggedIn', function (user, team) {
+        bot = user;
+        console.log('Logged in as ' + user.name + ' of ' + team.name + ', but not yet connected');
+    });
+
+    slackClient.on('open', function () {
+        console.log('Connected');
+    });
+
+    slackClient.on('message', function (message) {
+        if (message.user == bot.id) {
+            return;
+        } // Ignore bot's own messages
+
+        var channel = slackClient.getChannelGroupOrDMByID(message.channel);
+        channel.send('Io non ho capito!');
+
+        // More goes here later..
+    });
+
+    slackClient.login();
+
+};
+
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var cors = require('cors');
+var corsOptions = {
+    origin:         '*',
+    exposedHeaders: ['totalItems'],
+    methods:        ['GET', 'PUT', 'POST', 'DELETE']
+};
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(tommy);
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '15mb'}));
+
+
+if (process.argv[2] === 'debug') {
+    process.env.NODE_VERBOSE = true;
+}
+
+
+app.listen(function () {
+    console.log('Started  HTTP server on ' + app.get('port'));
+});
+
