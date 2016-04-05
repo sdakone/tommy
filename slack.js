5f902288b87b284448aad97b251fb38b03353538
Alberto Urbano <hunterxyz@gmail.com>
2016-04-05 15:36:34 +0200

    random limit

--- slack.js
+++ slack.js
@@ -4,9 +4,10 @@ var slackClient = require('slack-client');
 var RtmClient = slackClient.RtmClient;
 var RTM_EVENTS = slackClient.RTM_EVENTS;
 
-var messagesLimit = 10;
+var messagesLimit = parseInt((Math.random() * 10) + 5, 10);
+console.log('Next message in ' + messagesLimit);
 var channels = {
-    tombot: 'C0XNDQLE7',
+    tombot:  'C0XNDQLE7',
     general: 'C0WS5Q31N'
 };
 
@@ -17,21 +18,28 @@ rtm.start();
 var messageCount = 0;
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 
-    messageCount++;
     var channelId = message.channel;
-    console.log(channelId);
-    if (channelId === channels.general && messageCount === messagesLimit) {
 
-        messageCount = 0;
-        rtm.sendMessage('Non ho capito! :/',channelId);
+    if (channelId === channels.general){
+        messageCount++;
+
+        if (messageCount === messagesLimit) {
+
+            messageCount = 0;
+            messagesLimit = parseInt((Math.random() * 10) + 5, 10);
+            console.log('Next message in ' + messagesLimit);
+            rtm.sendMessage('Non ho capito! :/', channelId);
+
+        }
 
     }
+
 });
 
-rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded() {
     //console.log('Reaction added:', reaction);
 });
 
-rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved() {
     //console.log('Reaction removed:', reaction);
 });
