1884e82617c7cb6dab8ec04907e2e3b909a85a34
OpenShift System User <5704e6312d527173c50000d5@ex-std-node133.prod.rhcloud.com>
2016-04-06 13:25:27 -0400

    fix

--- run_it.sh
+++ run_it.sh
@@ -5,5 +5,5 @@ echo “git pull”
  cd /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy
  git pull 
 
-echo “node slack”
+#echo “node slack”
 node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js
