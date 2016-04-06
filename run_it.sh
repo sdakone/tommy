266520fee5425c88c77bc985d6f725ad1dfbe900
sdakone <davidetantillo@Davide-Tantillo.local>
2016-04-06 19:07:35 +0200

    fix it

--- run_it.sh
+++ run_it.sh
@@ -2,7 +2,8 @@ echo “kill node”
 killall node
 
 echo “git pull”
-git pull
+ cd /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy
+ git pull 
 
 echo “node slack”
-node slack.js
+node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js
