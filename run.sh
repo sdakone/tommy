3dcd854d0d9caf612899c205722e88641effc271
OpenShift System User <5704e6312d527173c50000d5@ex-std-node133.prod.rhcloud.com>
2016-04-07 07:01:58 -0400

    edited the script for backgrond

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy && git pull
 killall node
 echo "launching node" 
-node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js
+node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js &
