202acc8635dc0567a48b625c2861f139a5ddc76a
OpenShift System User <5704e6312d527173c50000d5@ex-std-node133.prod.rhcloud.com>
2016-04-07 08:59:25 -0400

    fix script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy && git pull
 killall node
-echo "launching node" 
-node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js &
+echo "launching node"
+nohup node /var/lib/openshift/5704e6312d527173c50000d5/app-deployments/testapp/tommy/slack.js 0<&- &>/dev/null & 
