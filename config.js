5c78faf4d082ccf4ba10c1b2fd1c6b89290c7339
Alberto Urbano <hunterxyz@gmail.com>
2016-04-08 11:12:38 +0200

    bots folder

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports =
+{
+    channels: {
+        tombot:  'C0XNDQLE7',
+        general: 'C0WS5Q31N'
+    },
+
+    bots: {
+        tommaso: 'xoxb-31735152998-FyaXBNNVWzJ02aqkbqpWtp5t'
+    }
+
+};
--- slack.js
+++ slack.js
@@ -1,62 +1,14 @@
 'use strict';
 
-var nextLimit = function () {
+var config = require('./config');
 
-    return parseInt((Math.random() * 5) + 5, 10);
+for (var bot in config.bots) {
 
-};
+    if (config.bots.hasOwnProperty(bot)) {
 
-var channels = {
-    tombot:  'C0XNDQLE7',
-    general: 'C0WS5Q31N'
-};
+        var rtmBot = require('./bots/' + bot);
 
-var slackClient = require('slack-client');
-var RtmClient = slackClient.RtmClient;
-var RTM_EVENTS = slackClient.RTM_EVENTS;
-var CLIENT_EVENTS = slackClient.CLIENT_EVENTS.RTM;
-var token = process.env.SLACK_API_TOKEN || 'xoxb-31735152998-FyaXBNNVWzJ02aqkbqpWtp5t';
-var rtm = new RtmClient(token);
-
-var messagesLimit = nextLimit();
-var messageCount = 0;
-
-rtm.on(CLIENT_EVENTS.RTM_CONNECTION_OPENED, function handleRtmMessage() {
-
-    rtm.sendMessage('Next message in.... ' + messagesLimit + ' :+1::skin-tone-3:', channels.tombot);
-
-});
-
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-
-    var channelId = message.channel;
-
-    if (message.text.indexOf('<@U0XMM4GVC>') !== -1) {
-        rtm.sendMessage('<@' + message.user + '> comu sini?', channelId);
-    }
-
-    if (channelId === channels.general) {
-        messageCount++;
-
-        if (messageCount === messagesLimit) {
-
-            messageCount = 0;
-            messagesLimit = nextLimit();
-            rtm.sendMessage('Next message in.... ' + messagesLimit + ' :+1::skin-tone-2:', channels.tombot);
-            rtm.sendMessage('Non ho capito! :confused:', channelId);
-
-        }
+        rtmBot.start();
 
     }
-
-});
-
-rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded() {
-    //console.log('Reaction added:', reaction);
-});
-
-rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved() {
-    //console.log('Reaction removed:', reaction);
-});
-
-rtm.start();
+}
