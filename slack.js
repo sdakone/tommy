0e6b51f7ea442cdf3038818873ac3ecf16960182
Alberto Urbano <hunterxyz@gmail.com>
2016-04-08 14:59:06 +0200

    wrong directory

--- slack.js
+++ slack.js
@@ -2,21 +2,22 @@
 
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 http.createServer(function () {}).listen(process.env.PORT || 5000);
 
-var botsDir = './bots';
+var botsDir = path.join(__dirname, './bots');
 
 fs.readdir(botsDir, function (error, files) {
 
     for (var i = 0; i < files.length; i++) {
-        
+
         var bot = files[i];
         var rtmBot = require('./bots/' + bot);
 
         rtmBot.start();
-        console.log('Starting ',bot,' bot...');
-        
+        console.log('Starting ', bot, ' bot...');
+
     }
-    
+
 });
