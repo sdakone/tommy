8f833cb197bb316a7890d4f5c428e9128274ac61
sdakone <davidetantillo@Davide-Tantillo.local>
2016-04-08 12:22:11 +0200

    fixed the listening server

--- slack.js
+++ slack.js
@@ -4,27 +4,6 @@ var server = http.createServer(function(req, res) {
 
 }).listen(process.env.PORT || 5000);
 
-/*
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});*/
-
-
 'use strict';
 
 
